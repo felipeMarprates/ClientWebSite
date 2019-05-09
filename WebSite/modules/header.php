312e0b95ead79ee089e8cf1a85d906e91273479a
MARPRATES FELIPE <702030207>
2019-05-09 14:43:49 -0700

    settup colors in css and creating folder and files

---
+++ WebSite/modules/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <style href="WebSite/layout.css"></style>
+
+    
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+<ul>
+  <li><a href="?page=home"> Home</a></li>
+  <li><a href="?page=assingments"> assingments</a></li>
+  <li><a href="?page=lectures"> Lectures</a></li>
+  <li><a href="?page=about_me"> About me</a></li>
+</ul>
