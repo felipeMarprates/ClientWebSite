8c44541d5ea24b0a1df63cfb44bbdd1808a0d96f
MARPRATES FELIPE <702030207>
2019-05-17 15:15:41 -0700

    test to php
    Co-Authored-By: Jacob Johnson <jjohnson316@users.noreply.github.com>

--- WebSite/content/assingments.php
+++ WebSite/content/assingments.php
@@ -1,3 +1,81 @@
 <main>
-  
+  <div class="side-right1">
+  <p>Lorem ipsum dolo
+    r sit amet, consectetur
+    adipisicing elit. Consequatur, at!
+     Incidunt aliquid similique
+     rerum voluptatibus amet
+    provident, voluptatum repellendus
+     unde odio in, quo voluptates
+     doloribus sed corporis totam explicabo
+     velit.</p>
+
+
+
+
+  </div>
+
+  <div class="side-right2">
+  <p>Lorem ipsum dolo
+    r sit amet, consectetur
+    adipisicing elit. Consequatur, at!
+
+     velit.</p>
+  </div>
+<div class="side-left">
+<p>Lorem ipsum dolo
+  r sit amet, consectetur
+  adipisicing elit. Consequatur, at!
+   Incidunt aliquid similique
+   rerum voluptatibus amet
+  provident, voluptatum repellendus
+   unde odio in, quo voluptates
+   doloribus sed corporis totam explicabo
+   velit. Lorem ipsum dolor sit amet, consectetur
+    adipisicing elit. Dolor eligendi aspernatur tempora,
+     illo quidem hic, est iure totam maiores fuga molestias
+      in excepturi dolore fugiat voluptas assumenda omnis
+      debitis velit.Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Nisi voluptatum vero, voluptate quisquam
+      dolorem atque neque, illum repellendus aspernatur cum ipsam.
+       Temporibus consectetur vitae neque. Nulla deserunt ut delectus
+        ratione.</p>
+
+
+
+
+</div>
+<div class="padding-right-25 padding-left-25 padding-top-5 background" style="font-size:1.1vw;">
+  <h1> Hello welcome!</h1>
+  <p class=""> Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. Aut, necessitatibus soluta eveniet iusto omnis cumque
+    pariatur, consectetur ipsam doloribus veritatis nemo nam laudantium, natus blanditiis
+    quis explicabo nisi neque est.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Excepturi dolores ad accusamus voluptatibus cupiditate nemo nostrum blanditiis fugit
+    reiciendis rerum, quidem sequi vitae voluptas illo repudiandae. Delectus aliquam incidunt,
+    quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quo velit et cum,
+     quos deleniti recusandae ipsum commodi! Consequuntur aut ipsa ab doloribus explicabo adipisci
+     impedit, cupiditate accusantium placeat architecto!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+     Reprehenderit accusamus iste explicabo ea, reiciendis nihil corporis, sequi enim natus expedita autem
+    atque magni
+     commodi alias ipsum ducimus! Sequi, aperiam, optio!Lorem ipsum
+      dolor sit amet, consectetur adipisicing elit. Consectetur necessitatibus officia, fuga quis. Provident ipsa
+      m dolores minus quaerat corporis sapiente dolorem, omnis modi quam debitis fuga nostrum, maxime velit doloru
+      m?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque vel aut nulla soluta consequuntur nisi dist
+      inctio at laborum inventore blanditiis autem obcaecati sit, quibusdam enim quam deserunt et, accusantium anim
+      i.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque adipisci necessitatibus officia, molestiae
+      harum sunt asperiores magnam similique ipsam dolor possimus minima? Lorem ipsum dolor sit amet, consectetur a
+      dipisicing elit. Doloremque dolorum, consectetur dolore, cumque impedit commodi blanditiis voluptatibus lauda
+      ntium dicta quo quia nesciunt reiciendis ducimus illum tempora consequuntur minima incidunt natus. At perspic
+      iatis doloribus inventore laudantium dolore veritatis, nobis!Lorem ipsum dolor sit amet, consectetur adipisicing
+       elit. Doloribus rerum amet ducimus porro ab facili
+      s ut animi commodi illo provident, dolor magni magnam illum minima adipisci autem eos libero nesciunt.Lorem i
+      psum dolor sit amet, consectetur adipisicing elit. Consequatur aliquid unde qui repellat ratione sint. Corporis
+      ad quod ab culpa voluptatibus, labore suscipit laborum facere sequi non alias rerum delectus.</p>
+</div>
+
+
+
+
+
 </main>
